d506928d6e85647f566eb9c4d83811db3e2f60c8
Nastsi <nastsi@mail.ru>
2017-09-29 18:17:01 +0600

    Add Compare
    Добавлена функция второго прохода по массиву для подсчета количества чисел равных минимальному

--- Laba_PA_1/Laba_PA_1/Program.cs
+++ Laba_PA_1/Laba_PA_1/Program.cs
@@ -21,6 +21,8 @@ namespace Laba_PA_1
         static public bool[] good;
         static public bool allGood;
         static public int done;
+        static public int[] result;
+        static public double times;
 
         class AllThreadsBorders
         {
@@ -47,7 +49,11 @@ namespace Laba_PA_1
                 borders[i].NumOfProcessor = i + 1;
                 good[i] = false;
             }
-
+            result = new int[threads];
+            for (int p = 0; p < threads; p++)
+            {
+                result[p] = 0;
+            }
             Stopwatch Time = new Stopwatch();
             Time.Start();
             for (int i = 0; i < threads; i++)
@@ -82,19 +88,6 @@ namespace Laba_PA_1
                 Thread.Sleep(10);
             }
 
-            Console.Write("mass: ");
-            for (int i = 0; i< mass.Length; i++)
-            {
-                Console.Write(mass[i] + " ");
-            }
-
-            Console.WriteLine();
-            Console.Write("Minimums: ");
-            for (int i = 0; i < min.Length; i++)
-            {
-                Console.Write(min[i] + " ");
-            }
-
             minimum = min[0];
             for (int i = 1; i < threads; i++)
             {
@@ -103,8 +96,48 @@ namespace Laba_PA_1
                     minimum = min[i];
                 }
             }
-            Console.WriteLine();
-            Console.WriteLine(minimum);
+
+            Thread[] AllThreads2 = new Thread[threads];
+            allGood = false;
+            done = 0;
+            for (int i = 0; i < threads; i++)
+            {
+                AllThreads2[i] = new Thread(new ParameterizedThreadStart(Compare));
+                good[i] = false;
+            }
+            for (int i = 0; i < threads; i++)
+            {
+                AllThreads2[i].Start(borders[i]);
+            }
+            while (!allGood)
+            {
+                bool now = true;
+                for (int i = 0; i < threads; i++)
+                {
+                    if (good[i] == false)
+                    {
+                        now = false;
+                        break;
+                    }
+                }
+                if (now == true)
+                {
+                    allGood = true;
+                }
+                else
+                {
+                    Thread.Sleep(10);
+                }
+            }
+            AllThreads2[threads - 1].Join();
+
+            while (done != threads)
+            {
+                Thread.Sleep(10);
+            }
+            Time.Stop();
+            times = Time.Elapsed.TotalMilliseconds;
+
         }
 
         public static void FindingMin(object borders)
@@ -116,12 +149,25 @@ namespace Laba_PA_1
                 int id = GetCurrentThreadId();
                 if (id == process.Id)
                 {
-                    for (int i = 1; i <= threads; i++)
+                    if (border.NumOfProcessor == 1)
                     {
-                        if (border.NumOfProcessor == i)
-                        {
-                            process.ProcessorAffinity = (IntPtr)(i);
-                        }
+                        process.ProcessorAffinity = (IntPtr)(1);
+                    }
+                    else if (border.NumOfProcessor == 2)
+                    {
+                        process.ProcessorAffinity = (IntPtr)(2);
+                    }
+                    else if (border.NumOfProcessor == 3)
+                    {
+                        process.ProcessorAffinity = (IntPtr)(4);
+                    }
+                    else if (border.NumOfProcessor == 4)
+                    {
+                        process.ProcessorAffinity = (IntPtr)(8);
+                    }
+                    else if (border.NumOfProcessor == 5)
+                    {
+                        process.ProcessorAffinity = (IntPtr)(15);
                     }
                 }
             }
@@ -136,7 +182,7 @@ namespace Laba_PA_1
 
             for (int i = border.Left; i <= border.Right; i++)
             {
-               if (mass[i] < min[border.NumOfProcessor - 1])
+                if (mass[i] < min[border.NumOfProcessor - 1])
                 {
                     min[border.NumOfProcessor - 1] = mass[i];
                 }
@@ -144,58 +190,93 @@ namespace Laba_PA_1
             done++;
         }
 
+        public static void Compare(object borders)
+        {
+            AllThreadsBorders border = (AllThreadsBorders)borders;
+
+            foreach (ProcessThread process in Process.GetCurrentProcess().Threads)
+            {
+                int id = GetCurrentThreadId();
+                if (id == process.Id)
+                {
+                    if (border.NumOfProcessor == 1)
+                    {
+                        process.ProcessorAffinity = (IntPtr)(1);
+                    }
+                    else if (border.NumOfProcessor == 2)
+                    {
+                        process.ProcessorAffinity = (IntPtr)(2);
+                    }
+                    else if (border.NumOfProcessor == 3)
+                    {
+                        process.ProcessorAffinity = (IntPtr)(4);
+                    }
+                    else if (border.NumOfProcessor == 4)
+                    {
+                        process.ProcessorAffinity = (IntPtr)(8);
+                    }
+                    else if (border.NumOfProcessor == 5)
+                    {
+                        process.ProcessorAffinity = (IntPtr)(15);
+                    }
+                }
+            }
+            good[border.NumOfProcessor - 1] = true;
+
+            while (!allGood)
+            {
+                Thread.Sleep(10);
+            }
+
+            for (int i = border.Left; i <= border.Right; i++)
+            {
+                if (mass[i] == min[border.NumOfProcessor - 1])
+                {
+                    result[border.NumOfProcessor - 1]++;
+                }
+            }
+            done++;
+        }
+
         static void Main(string[] args)
         {
-            long n = 1000000;
+            long n = 300000000;
             Random rand = new Random();
             int key = 0;
             threads = 0;
 
             mass = new int[n];
-            for(int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
-                mass[i] = rand.Next(5, 1000);
+                mass[i] = rand.Next(5, 100);
             }
 
             do
             {
                 Console.WriteLine();
-                Console.WriteLine("1. Enter size of massive.");
-                Console.WriteLine("2. Enter numbers of threads.");
-                Console.WriteLine("3. Start.");
-                Console.WriteLine("4. Exit.");
+                Console.WriteLine("1. Start.");
+                Console.WriteLine("2. Exit.");
                 Console.WriteLine();
                 Console.Write("Answer: ");
                 int.TryParse(Console.ReadLine(), out key);
-                
+
                 if (key == 1)
                 {
-                    Console.Clear();
-                    Console.WriteLine();
-                    Console.Write("Enter size of massive: ");
-                    long.TryParse(Console.ReadLine(), out n);
-                    for (int i = 0; i < n; i++)
+                    for (int i = 1; i <= 5; i++)
                     {
-                        mass[i] = rand.Next(0, 100);
-                    }
-                    Console.Clear();
+                        threads = i;
+                        Start();
+                        int tek2 = 0;
+                        for (int p = 0; p < threads; p++)
+                        {
+                            tek2 += result[p];
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine("Time: " + times + ". Result: " + tek2);
+                    }                    
                 }
 
-                if (key == 2)
-                {
-                    Console.Clear();
-                    Console.WriteLine();
-                    Console.Write("Enter number of threads: ");
-                    int.TryParse(Console.ReadLine(), out threads);
-                    Console.Clear();
-                }
-
-                if (key == 3)
-                {
-                    Start();
-                }
-
-            } while (key != 4);
+            } while (key != 2);
         }
     }
 }
